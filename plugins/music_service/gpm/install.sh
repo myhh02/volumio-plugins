3314647e8d1074451fa96faa25896cc5b63eef30
vanita5 <mail@vanit.as>
2017-02-09 09:55:10 +0100

    [gpm] installation script for GMusicProxy

--- plugins/music_service/gpm/install.sh
+++ plugins/music_service/gpm/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "TODO"
+echo "Installing GMusicProxy dependencies..."
+sudo apt-get update
+sudo apt-get -y install build-essential unzip python2.7-dev python-pip libffi-dev libssl-dev --no-install-recommends
+
+echo "Installing GMusicProxy..."
+cd /data
+wget https://github.com/diraimondo/gmusicproxy/archive/v1.0.8.zip
+sudo unzip v1.0.8.zip
+rm v1.0.8.zip
+cd gmusicproxy-1.0.8
+sudo pip install -r requirements.txt
+# Fix for ImportError: cannot import name InsecureRequestWarning
+sudo pip install --upgrade requests
 
 echo "plugininstallend"
