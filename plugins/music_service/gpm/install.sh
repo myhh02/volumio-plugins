db8e3fc26ee7d7303933d73a8e97cb12c752f69a
vanita5 <mail@vanit.as>
2017-02-09 17:46:39 +0100

    [gpm] install: fix incompatibility between pip and the current requests version

--- plugins/music_service/gpm/install.sh
+++ plugins/music_service/gpm/install.sh
@@ -2,7 +2,12 @@
 
 echo "Installing GMusicProxy dependencies..."
 sudo apt-get update
-sudo apt-get -y install build-essential unzip python2.7-dev python-pip libffi-dev libssl-dev --no-install-recommends
+sudo apt-get -y install build-essential unzip python2.7-dev libffi-dev libssl-dev --no-install-recommends
+
+# fix for incompatible pip and requests version, as the repository version is too old
+echo "Install current version of pip..."
+sudo apt-get remove python-pip
+sudo easy_install pip
 
 echo "Installing GMusicProxy..."
 cd /data
@@ -11,7 +16,5 @@ sudo unzip v1.0.8.zip
 rm v1.0.8.zip
 cd gmusicproxy-1.0.8
 sudo pip install -r requirements.txt
-# Fix for ImportError: cannot import name InsecureRequestWarning
-sudo pip install --upgrade requests
 
 echo "plugininstallend"
